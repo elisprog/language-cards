f45d0dbd3b3365e3942212b17467a78c12c78d17
Elizaveta <bogdanovaelis@gmail.com>
2024-02-18 22:40:17 +0300

    second commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card (){
+return (
+<div>
+    <p>word</p>
+    <p>перевод</p>
+    <p>тема</p>
+    </div>
+);
+}
+export default Card;
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card.jsx";
+function Main() {
+    return (
+        <div>
+        <h1>Списки слов и карточек</h1>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
+        );
+    }
+export default Main;
